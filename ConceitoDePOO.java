3909da837d372026c9b763527fe3f3ce3da38340
Kauã Reis R. <kaua732153@gmail.com>
2025-04-08 00:47:15 -0300

    commit para finalizar o dia de 07/04!

---
+++ ConceitoDePOO.java
@@ -0,0 +1,14 @@
+/*
+ !Programação estruturada:
+ ?  É um paradigma que visa melhorar a clareza, a qualidade e o tempo de desenvolvimento de um 
+ ?  programa de computador, fazendo uso extensivo das construções de fluxo de  controle estruturado
+ ?  de seleção (if/then/else) e repetição (while e for), estruturas de bloco e sub-rotinas.
+ ?    o que se deve ter em mente é que na programação estruturada implementamos algoritmos com
+ ?    estruturas sequencias denominados de procedimentos lineares, podendo afetar o valor das variáveis
+ ?    de escopo local ou global em uma aplicação.
+ !Programação orientada a objetos:
+ ?   POO é baseado no conceito de "objetos" que podem conter dados na forma de campos, também conhecidos
+ ?   como atributos, e códigos na forma de procedimentos, também conhecidos como métodos.
+ *Enquanto a programação estruturada é voltada a procedimentos e funções definidas pelo usuário,
+ *  A programação orientada a objetos é voltada a conceitos como o de classes e objetos.
+*/
